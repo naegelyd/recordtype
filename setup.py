9d4d2c97c3629c0921465bc61d8e390eb075594a
Eric V. Smith <eric@trueblade.com>
2011-10-12 14:37:47 -0400

    Initial version.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(name='recordtype',
+      version='0.1',
+      url='http://www.trueblade.com/recordtype',
+      author='Eric V. Smith',
+      author_email='eric@trueblade.com',
+      description='Similar to namedtuple, but instances are mutable.',
+      long_description=open('README.txt').read(),
+      classifiers=['Development Status :: 4 - Beta',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approced :: Apache Public Software License',
+                   'Topic :: Software Development :: Libraries :: Python Modules',
+                   ],
+      license='LICENSE.txt',
+      py_modules=['recordtype']
+      )
